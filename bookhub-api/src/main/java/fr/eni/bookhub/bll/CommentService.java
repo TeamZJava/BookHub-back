8fb112d625584a5ef76284035df0d6fc8fc09ab5
TeamZJava <githubjava@tutamail.com>
2026-04-24 02:02:28 +0200

    Update CommentService.java

--- bookhub-api/src/main/java/fr/eni/bookhub/bll/CommentService.java
+++ bookhub-api/src/main/java/fr/eni/bookhub/bll/CommentService.java
@@ -1,6 +1,16 @@
 package fr.eni.bookhub.bll;
 
+import fr.eni.bookhub.bo.Comment;
+
+import java.util.List;
+
 public interface CommentService {
 
     void addComment(int userId, int bookId, String text);
+
+    void signalerCommentaire(int commentId);
+
+    List<Comment> getCommentairesSignales();
+
+    void supprimerCommentaire(int commentId);
 }
